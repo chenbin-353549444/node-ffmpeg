cff3382a2b43ef8d3e9cfa8aa4c79ce6864eb1bd
ChenBin <353549444@qq.com>
2019-05-29 09:29:03 +0800

    :sparkles: 添加语音转换

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 let express = require('express');
+let FfmpegCommand = require('fluent-ffmpeg');
 let fs = require('fs');
 let app = express();
 let AipSpeech = require("baidu-aip-sdk").speech;
@@ -21,6 +22,30 @@ app.post('/file_upload', function (req, res) {
     console.log(req.files[0]);  // 上传的文件信息
     fs.readFile( req.files[0].path, function (err, data) {
         let voiceBase64 = new Buffer(data);
+        client.recognize(voiceBase64, 'pcm', 16000).then(function(result) {
+            res.end(JSON.stringify(result));
+        }, function(err) {
+            res.end(err);
+        });
+    });
+});
+
+app.post('/file_transform', function (req, res) {
+    res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+    console.log(req.files[0]);  // 上传的文件信息
+    var command = ffmpeg(fs.createReadStream(req.files[0].path))
+        .audioCodec('pcm_s16le')
+        .audioBitrate('16k')
+        .audioChannels(1)
+        .on('error', function(err) {
+            console.log('An error occurred: ' + err.message);
+        })
+        .on('end', function() {
+            console.log('Processing finished !');
+        });
+    var ffstream = command.pipe();
+    ffstream.on('data', function(chunk) {
+        let voiceBase64 = new Buffer(chunk);
         client.recognize(voiceBase64, 'pcm', 16000).then(function(result) {
             res.end(JSON.stringify(result));
         }, function(err) {
